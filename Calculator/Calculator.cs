f8b940cb904d5f9e91217a61842d449b42d5437d
MarcusKluver1 <marcus.kluver@gmail.com>
2021-02-04 14:18:14 +0100

    tilføjet kommentar

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -11,7 +11,7 @@ namespace Calculator
 
         public double Subtract(double a, double b)
         {
-            return a - b;
+            return a - b; // subtract betyder minus
         }
 
         public double Multiply(double a, double b)
