faae08ef77bfc3e662440b6b97a5649da2d7d640
MarcusKluver1 <marcus.kluver@gmail.com>
2021-02-09 17:32:45 +0100

    Lavet Divide, og tilføjet kommentarer til mulige ændringer.

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -24,6 +24,18 @@ namespace Calculator
             return Math.Pow(x, exp);
         }
 
+        public double Divide(double a, double b)
+        { 
+            //Dette nedenstående kode er til at prøve med en exception.
+
+            //if (a==0 || b==0)
+            //{
+            //    throw new DivideByZeroException();
+            //}
+
+            return a / b;
+        }
+
         // mikkle tilføjet kommentar
     }
 }
